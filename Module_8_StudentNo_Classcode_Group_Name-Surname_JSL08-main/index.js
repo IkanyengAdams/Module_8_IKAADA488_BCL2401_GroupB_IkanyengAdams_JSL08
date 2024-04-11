5d49b4f17e4638f3e1ae683fc2bd6defcf5a0361
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-04-11 10:52:43 +0200

    defined a variable to hold a singleton instance

--- Module_8_StudentNo_Classcode_Group_Name-Surname_JSL08-main/index.js
+++ Module_8_StudentNo_Classcode_Group_Name-Surname_JSL08-main/index.js
@@ -1,7 +1,7 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-
+let bankBranchInstance = null;
 // 2. Define a class called `BankBranch` for managing branch information.
 
 // 3. In the `BankBranch` class:
@@ -32,3 +32,4 @@
 // - The `getBranchInfo` method allows us to retrieve the branch information from the singleton instance.
 // - We demonstrate the Singleton pattern by creating two instances (`branchA` and `branchB`) and show that they both point to the same instance,
 //   ensuring that there is only one set of branch information throughout the application.
+ 
